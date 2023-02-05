334a08db9e168343bc3b3e811b50fe5e994ef8db
justObd <117019547+justObd@users.noreply.github.com>
2023-02-05 21:33:31 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("hi my name is abdullah abdulrahim justaniah and i am junior in king abdulaziz university");
+        System.out.println("hi my name is abdullah abdulrahim justaniah and i am senior in king abdulaziz university");
         System.out.println("hi this is just practicing to pull and push project to this repositry");
     Scanner sc = new Scanner (System.in);
     System.out.println("enter your name");
