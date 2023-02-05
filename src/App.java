83755a0036a5e4a1d720b11b106721f50e1f034f
abdullah <abood1just@gmail.com>
2023-02-05 22:54:42 +0300

    modification in the irrelvant file

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("hi my name is abdullah abdulrahim justaniah and i am senior in king abdulaziz university");
@@ -8,8 +13,17 @@ public class App {
     Scanner sc = new Scanner (System.in);
     System.out.println("enter your name");
     String name = sc.nextLine();
-    System.out.println("hello "+name)
+    System.out.println("hello "+name);
         //this code is not neded in the project
+ArrayList a =new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            extracted(a, i);
+        }
         
+    }
+
+    private static boolean extracted(ArrayList a, int i) {
+        return a.add(i);
     }
 }
