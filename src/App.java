99de0e6b5011b6c841a095d2643e55b2dd302967
justObd <117019547+justObd@users.noreply.github.com>
2023-02-05 21:07:27 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("hi my name is abdullah abdulrahim justaniah and i am junior in king abdulaziz university");
+        
     }
 }
