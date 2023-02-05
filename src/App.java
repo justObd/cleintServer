43ad830d726e09c728828dff52aa1c62bbb50187
abdullah <abood1just@gmail.com>
2023-02-05 21:32:22 +0300

    this is the second time of me tring to push

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("hi my name is abdullah abdulrahim justaniah and i am junior in king abdulaziz university");
-        
+        System.out.println("hi this is just practicing to pull and push project to this repositry");
+    Scanner sc = new Scanner (System.in);
+    System.out.println("enter your name");
+    String name = sc.nextLine();
+    System.out.println("hello "+name);
     }
 }
