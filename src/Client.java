7eae2ce8916a4a98dd3e5b6715f9fc0fd1da588f
abdullah <abood1just@gmail.com>
2023-02-07 19:27:44 +0300

    the final updates

--- src/Client.java
+++ src/Client.java
@@ -4,50 +4,65 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
+    private static Connection connection;
     public static void main(String[] args) throws IOException, SQLException {
         String line;
         String ClientInput;
-            // -----------------------------------------------------------------------------------------------------------
+            
         try {
             Socket client = new Socket("localhost", 2000); // it will connect the client with the server
-            // -----------------------------------------------------------------------------------------------------------
+            
             InputStream in = client.getInputStream();
             OutputStream out = client.getOutputStream();
             Scanner sc = new Scanner(System.in);                                 
             Scanner receiver = new Scanner(in);
             PrintWriter writer = new PrintWriter(out, true);
-            System.out.println("first part completed ");
-            // -----------------------------------------------------------------------------------------------------------
+            testDatabaseConnection();//test the database connetion
+            System.out.println("first part completed ");//for debug
+            
             ArrayList<String> inp = new ArrayList<String>();
             inp.add("Enter username:");
             inp.add("Enter Password:");
             inp.add("choose: ");
-            inp.add("Which room do you want to update?");      // This arrayList is a stop sign.
-            inp.add("Enter check in date DD/MM: ");            //If the server sent one of these things
-            inp.add("Update visitor name");                    //it will stop receiving and it will send input to the server.
+            inp.add("Which room do you want to update?");      
+            inp.add("Enter check in date DD/MM: ");            
+            inp.add("Update visitor name");                    
             inp.add("Enter check out date DD/MM: ");
             inp.add("ENTER ROOM NO: ");
-            System.out.println("second part completed");
+            System.out.println("second part completed");//for debug
             System.out.println("  "); //just for practice gitHub
 
-            // -----------------------------------------------------------------------------------------------------------
+            
             while (true) {
                 line = receiver.nextLine();
                 System.out.println(line);
-                if (inp.contains(line)) {                               // In this loop, it will receive from the server and it will print it. 
-                    ClientInput = sc.nextLine();                        // After that, it will check if the thing that have been sent was one of the 
-                    writer.println(ClientInput);                        // Stop signs, if it was. It will stop to send an input to the server.
+                if (inp.contains(line)) {                              
+                    ClientInput = sc.nextLine();                       
+                    writer.println(ClientInput);                        
                 }
             }
 
-        } catch (ConnectException e) { // if the server was offline it will handel it
+        } catch (ConnectException e) { // if the server was offline it this exception will handel it
             System.out.println("Server is offline");
             System.out.println("eceeption occured o");
         }
     }
+    private static void closeDatabaseConnection() throws SQLException  {
+        connection.close();  
+   
+       }
+   
+       
+       private static void testDatabaseConnection() throws SQLException{
+          connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/client","root","123");
+          System.out.println("Database connection is valid : "+connection.isValid(5));
+   
+       }
 }
